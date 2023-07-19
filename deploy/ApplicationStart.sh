59678bd72cd3946dbda277929156313927700238
Prashanth <123293783+prashanth2520@users.noreply.github.com>
2023-07-19 17:45:29 +0530

    Update ApplicationStart.sh

--- deploy/ApplicationStart.sh
+++ deploy/ApplicationStart.sh
@@ -1,7 +1,6 @@
 #!bin/bash/
 # Goto the application path
 cd /opt/laravel/
-# Git pull & Execute artisan command
+# Git pull & Execute PHP artisan command
 git pull && php artisan cache:clear && php artisan config:clear && php artisan view:clear && php artisan route:clear
-# Restart the nginx service
-sudo service nginx restart
+
