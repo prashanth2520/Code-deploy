764c8e4e12939031b4493f6add1cde38801fd008
Prashanth <123293783+prashanth2520@users.noreply.github.com>
2023-07-19 22:10:14 +0530

    Update and rename ApplicationStart.sh to beforeinstall.sh

--- deploy/ApplicationStart.sh
+++
@@ -1,6 +0,0 @@
-#!bin/bash/
-# Goto the application path
-cd /opt/laravel/
-# Git pull & Execute PHP artisan command
-git pull && php artisan cache:clear && php artisan config:clear && php artisan view:clear && php artisan route:clear
-
