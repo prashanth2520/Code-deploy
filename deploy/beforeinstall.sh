dc9095ef42fb448e76f735af39c14716319e1a23
Prashanth <123293783+prashanth2520@users.noreply.github.com>
2023-07-21 17:28:00 +0530

    Update beforeinstall.sh

--- deploy/beforeinstall.sh
+++ deploy/beforeinstall.sh
@@ -1,7 +1,5 @@
 #!/bin/bash/
-Date=$(date +"%d-%m-%y")
-App-dir= /opt/laravel/
-cd $App-dir && zip -r Code-deploy-$Date.zip Code-deploy
-aws s3 mv Code-deploy-$Date.zip s3://demoplaylist0712/ --region ap-south-1
+cd /opt/laravel/
+sudo mkdir demo001
 
 
