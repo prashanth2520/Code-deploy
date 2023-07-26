b9f4f47dd0084a74047d29091d83f9388fc19c56
Prashanth <123293783+prashanth2520@users.noreply.github.com>
2023-07-26 22:40:25 +0530

    Update beforeinstall.sh

--- deploy/beforeinstall.sh
+++ deploy/beforeinstall.sh
@@ -3,6 +3,7 @@ path=/opt/laravel/Addval
 cd /opt/laravel
 sudo rm -r Addval
 sudo mkdir /opt/laravel/Addval
+sudo chown -R kevin:kevin Addval/
 sudo aws s3 cp s3://example2607/code-file.zip $path
 cd $path
 sudo unzip code-file.zip
