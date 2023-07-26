6b8d3cdd3bb252a0cc59fbfb1071547c675b30d2
Prashanth <123293783+prashanth2520@users.noreply.github.com>
2023-07-26 21:22:16 +0530

    Update beforeinstall.sh

--- deploy/beforeinstall.sh
+++ deploy/beforeinstall.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 path=/home/ubuntu/opt/laravel/
 cd $path
-rm -r code-file
+sudo rm -r code-file
 aws s3 cp s3://example2607/code-file.zip
-unzip code-file.zip
-rm -r code-file.zip
+sudo unzip code-file.zip
+sudo rm -r code-file.zip
 
 
 
